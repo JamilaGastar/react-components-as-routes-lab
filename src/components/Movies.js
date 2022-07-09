706ed9581f2ad31e180c5c17db86b4cd4cd427db
jamilagastar <jamilagastar@gmail.com>
2022-07-09 15:30:00 +1000

    completed lab

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,12 +1,27 @@
 import React from 'react';
 import { movies } from '../data';
 
-const Movies = () => {
-  return (
-    <div>
-        {/*{code here}*/}
-    </div>
-  );
+class Movies extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Movies Page</h1>
+        {movies.map((movie) => {
+        return (
+          <div>
+          <h4>Name: {movie.title}</h4>
+          <p>Time: {movie.time}</p>
+          Genres:
+          <ul>{movie.genres.map((genre) => { 
+            return <li>{genre}</li>;
+          })}
+          </ul>
+          </div>
+        );
+      })}
+      </div>
+    );
 };
+}
 
 export default Movies;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,8 +3,35 @@ import { NavLink } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>
-      {/*{code here}*/}
+    <div className='navbar'>
+      <NavLink
+          to="/"
+          exact
+          style={{
+            padding: "5px"
+          }}
+        >Home</NavLink>
+        <NavLink
+          to="/movies"
+          exact
+          style={{
+            padding: "5px"
+          }}
+        >Movies</NavLink>
+        <NavLink
+          to="/directors"
+          exact
+          style={{
+            padding: "5px"
+          }}
+        >Directors</NavLink>
+        <NavLink
+          to="/actors"
+          exact
+          style={{
+            padding: "5px"
+          }}
+        >Actors</NavLink>
     </div>
   );
 };
